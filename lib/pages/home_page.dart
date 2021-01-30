ce0d45011984ab9d75b6549133939734f38192dd
Daniel <raiga1234@gmail.com>
2021-01-30 10:48:29 -0500

    Add home and maps page, add auxiliar switch

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/pages/addresses_page.dart';
+import 'package:qr_reader/pages/maps_page.dart';
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
@@ -13,12 +15,25 @@ class HomePage extends StatelessWidget {
             IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})
           ],
         ),
-        body: Center(
-          child: Text('Home Page'),
-        ),
+        body: _HomePageBody(),
         bottomNavigationBar: CustomNavigationBar(),
         floatingActionButton: ScanButton(),
         floatingActionButtonLocation:
             FloatingActionButtonLocation.centerDocked);
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final currentIndex = 1;
+    switch (currentIndex) {
+      case 0:
+        return MapsPage();
+      case 1:
+        return AddressesPage();
+      default:
+        return MapsPage();
+    }
   }
 }
--- lib/widgets/custom_navigatorbar.dart
+++ lib/widgets/custom_navigatorbar.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class CustomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final currentIndex = 1;
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: currentIndex,
       elevation: 0,
       items: [
         BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
-        BottomNavigationBarItem(icon: Icon(Icons.compass_calibration), label: 'Address')
+        BottomNavigationBarItem(
+            icon: Icon(Icons.compass_calibration), label: 'Address')
       ],
     );
   }
