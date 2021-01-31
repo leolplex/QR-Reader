a542959746dff3376290da1f337ea1432c95a75c
Daniel <raiga1234@gmail.com>
2021-01-31 15:47:06 -0500

    Add manage status with provider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/addresses_page.dart';
 import 'package:qr_reader/pages/maps_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
@@ -26,7 +28,8 @@ class HomePage extends StatelessWidget {
 class _HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 1;
+    final uiProvider = Provider.of<UiProvider>(context);
+    final currentIndex = uiProvider.selectedMenuOpt;
     switch (currentIndex) {
       case 0:
         return MapsPage();
