35c962a13f040deb399b059682be83ff360d1e4b
Daniel <raiga1234@gmail.com>
2021-01-31 16:23:45 -0500

    Add model scan

---
+++ lib/models/scan_model.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+
+ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+
+String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+
+class ScanModel {
+  ScanModel({
+    this.id,
+    this.type,
+    @required this.value,
+  }) {
+    if (this.type.contains('http')) {
+      this.type = 'http';
+    } else {
+      this.type = 'geo';
+    }
+  }
+
+  int id;
+  String type;
+  String value;
+
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        type: json["type"],
+        value: json["value"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "type": type,
+        "value": value,
+      };
+}
