14414d3a7ea9d14900f94cf9b8f61fc1e564d77c
Daniel <raiga1234@gmail.com>
2021-02-01 20:39:42 -0500

    Add shell map

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Maps Page'),
-    );
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (_, i) => ListTile(
+              leading: Icon(
+                Icons.map,
+                color: Theme.of(context).primaryColor,
+              ),
+              title: Text('http://loquesea.com'),
+              subtitle: Text('ID: 1'),
+              trailing: Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.grey,
+              ),
+              onTap: () => print('Abrir algo'),
+            ));
   }
 }
---
+++ lib/providers/scan_list_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:qr_reader/models/scan_model.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+
+class ScanListProvider extends ChangeNotifier {
+  List<ScanModel> scans = [];
+  String selectedType = 'http';
+
+  newScan(String value) async {
+    final newScan = new ScanModel(value: value);
+    final id = await DBProvider.db.newScan(newScan);
+    newScan.id = id;
+    if (this.selectedType == newScan.type) {
+      this.scans.add(newScan);
+      notifyListeners();
+    }
+  }
+
+  loadScans() async {
+    final scans = await DBProvider.db.getAllScans();
+    this.scans = [...scans];
+    notifyListeners();
+  }
+
+  loadScansByType(String type) async {
+    final scans = await DBProvider.db.getScansByType(type);
+    this.scans = [...scans];
+    this.selectedType = type;
+    notifyListeners();
+  }
+
+  deleteAll() async {
+    await DBProvider.db.deleteAllScan();
+    this.scans = [];
+    notifyListeners();
+  }
+
+  deleteById(int scanId) async {
+    await DBProvider.db.deleteScan(scanId);
+    this.loadScansByType(this.selectedType);
+  }
+}
