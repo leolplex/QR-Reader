0df74ab3d77af17a6cc926e5b52fff65ea380197
Daniel <raiga1234@gmail.com>
2021-03-14 21:21:30 -0500

    Add button to change map type

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -11,27 +11,56 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   Completer<GoogleMapController> _controller = Completer();
+  MapType mapType = MapType.normal;
 
   @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
-    final CameraPosition startPoint = CameraPosition(
-      target: scan.getLatLng(),
-      zoom: 17,
-    );
+    final CameraPosition startPoint =
+        CameraPosition(target: scan.getLatLng(), zoom: 17, tilt: 50);
+
+    // Markets
+    Set<Marker> markers = new Set<Marker>();
+    markers.add(new Marker(
+        markerId: MarkerId('geo-location'), position: scan.getLatLng()));
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.location_searching_rounded),
+            onPressed: () async {
+              final GoogleMapController controller = await _controller.future;
+              controller.animateCamera(CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                      target: scan.getLatLng(), zoom: 15.5, tilt: 50)));
+            },
+          )
+        ],
       ),
       body: GoogleMap(
         myLocationButtonEnabled: false,
-        mapType: MapType.normal,
+        mapType: mapType,
+        markers: markers,
         initialCameraPosition: startPoint,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.layers),
+        onPressed: () {
+          setState(() {
+            if (mapType == MapType.normal) {
+              mapType = MapType.satellite;
+            } else {
+              mapType = MapType.normal;
+            }
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
     );
   }
 }
